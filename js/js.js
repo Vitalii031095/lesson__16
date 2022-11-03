e9a3d45728582de1aa1cc9f8e4718b1fcaf1deb6
Vitalii <112901030+Vitalii031095@users.noreply.github.com>
2022-11-03 14:40:41 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,10 @@
+// let wrapper = document.querySelector(".page")
+// window.addEventListener("scroll", function () {
+//    let q = window.scrollY;
+//    if (q > 0) {
+//       wrapper.classList.add("rot");
+//    }
+//    else {
+//       wrapper.classList.remove("rot");
+//    }
+// })
